a30eb60dd7fdbe9c3dc4e20b7ddba24de9c41730
davide <davideandreazzini@gmail.com>
2016-07-19 16:21:49 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+var Firebase = require('firebase');
+var ElasticClient = require('elasticsearchclient');
+
+var config = {
+    // firebase configuration here
+  };
+  Firebase.initializeApp(config);
+
+// initialize our ElasticSearch API
+var client = new ElasticClient({ host: 'localhost', port: 9200 });
+
+// listen for changes to Firebase data
+var fb = Firebase.database().ref('products');
+
+client.index('firebase', 'products', {name: 'test'}, 1)
+    .on('data', function(data) {
+        console.log(data)
+    })
+    .exec()
+
+var n = 0;
+var arr = [];
+var prods = {}
+fb.once('value').then(function(snapshot) {  
+    console.log('ok')
+    prods = snapshot.val();
+    arr = [];
+    n=0;
+    for(k in snapshot.val()){
+        arr.push(k)
+    }
+    console.log(arr.length)
+   console.log('end')
+   setInterval(function(){
+       console.log(n)
+       client.index('firebase', 'products', prods[arr[n]], arr[n])
+        .on('data', function(data) {
+            // console.log(data)
+        })
+        .exec()
+    
+
+       n++
+   },50)
+  
+  
+});
+
+
+fb.on('child_added',   createOrUpdateIndex);
+fb.on('child_changed', createOrUpdateIndex);
+fb.on('child_removed', removeIndex);
+
+function createOrUpdateIndex(snap) {
+    // console.log(snap.val(), snap.key )
+   client.index('firebase', 'products', snap.val(), snap.key)
+     .on('data', function(data) { console.log('indexed ', snap.key); })
+     .on('error', function(err) { 
+        console.log(err) 
+         });
+}
+
+function removeIndex(snap) {
+    // console.log(snap)
+   client.deleteDocument('firebase', 'products', snap.key, function(error, data) {
+      if( error ) console.error('failed to delete', snap.key, error);
+      else console.log('deleted', snap.key);
+   });
+}
+
+
---
+++ search.js
@@ -0,0 +1,60 @@
+var Firebase = require('firebase');
+var elasticsearch = require('elasticsearch');
+var client = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+});
+
+var config = {
+   // firebase configuration here
+};
+Firebase.initializeApp(config);
+
+
+// listen for requests at https://<INSTANCE>.firebaseio.com/search/request
+var queue = Firebase.database().ref('search');
+queue.child('request').on('child_added', processRequest);
+
+function processRequest(snap) {
+    console.log('start search')
+    console.log(snap.val())
+    var data = snap.val();
+    snap.ref.remove(); // clear the request after we receive it
+
+    // Query ElasticSearch
+    var bodyObj = {
+        query: {
+            bool: {
+                should: [
+                    { match: { name: data.query }, },
+                    { match: { summary: data.query } },
+                    { match: { category: data.query }, },
+                    { match: { main_category: data.query } }
+                    
+                ]
+            }
+        }
+    }
+
+    bodyObj = Object.assign(bodyObj, data.options)
+    client.search({
+        index: 'firebase',
+        type: 'products',
+        body: bodyObj
+    }).then(function (resp) {
+        var hits = resp.hits.hits;
+        queue.child('response/' + snap.key).set(resp);
+    }, function (err) {
+        console.trace(err.message);
+    });
+
+
+    //    client
+    //    .search(data.index, data.type, { "query": { 'query_string': { query: data.query } } })
+    //    .on('data', function(data) {
+    //        // Post the results to https://<INSTANCE>.firebaseio.com/search/response
+    //        queue.child('response/'+snap.key).set(results);
+    //    })
+    //    .on('error', function(error){ /* process errors */ })
+    //    .exec();
+}
