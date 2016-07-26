a0f473f82286241677a5d2c4a73e8aeae7334e12
davide <davideandreazzini@gmail.com>
2016-07-26 23:12:42 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var Firebase = require('firebase');
 var ElasticClient = require('elasticsearchclient');
 
 var config = {
-    // firebase configuration here
+    apiKey: "AIzaSyAG1u29W1E_Y3ZXr2ynUyC3dhee3NlbI_M",
+    authDomain: "grooco-3a6e4.firebaseapp.com",
+    databaseURL: "https://grooco-3a6e4.firebaseio.com",
+    storageBucket: "grooco-3a6e4.appspot.com",
   };
   Firebase.initializeApp(config);
 
